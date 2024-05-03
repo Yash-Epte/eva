9586765dce4ed934bf109d9ca9ae7430aa92127a
Yash Epte <121223452+Yash-Epte@users.noreply.github.com>
2024-05-03 11:56:32 +0530

    Update params.py

--- params.py
+++ params.py
@@ -1,4 +1,4 @@
-openai_api_key = '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'
+openai_api_key = 'sk-proj-duPTCuyTmY4zZ1CILJCIT3BlbkFJonamMa7NeF1q6iT3Gkrn'
 ACCESS_TOKEN = "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"  # Replace with your actual token
 mongodb_conn_string = 'mongodb+srv://epteyash777:7Yoam6fAnv0K8fHa@cluster0.jtm8eib.mongodb.net/'
 db_name = "search_db"
